183f3a1bfac0e47d8c538c34756d77a8196822a9
Guilherme de Almeida <guilherme.ampp@gmail.com>
2020-04-07 21:38:51 -0300

    Challenge 1: Node.js concepts complete!
    * GET /repositories
    * POST /repositories
    * PUT /repositories/:id
    * DELETE /repositories/:id
    * POST /repositories/:id/like
    
    All tests are passing!

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid } = require('uuidv4');
 
 const app = express();
 
@@ -11,23 +10,62 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { url, title, techs } = request.body;
+
+  const repo = {url, title, techs, likes: 0, id: uuid()}
+  repositories.push(repo);
+
+  return response.status(201).json(repo);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { url, title, techs } = request.body;
+
+  repoIndex = repositories.findIndex(repo => repo.id === id);
+  if (repoIndex < 0) {
+    return response.status(400).send();
+  }
+  const repo = repositories[repoIndex];
+  const updatedRepo = {
+    url: url ? url : repo.url, 
+    title: title ? title : repo.title,
+    techs: techs ? techs : repo.techs, 
+    likes: repo.likes, 
+    id: repo.id
+  };
+  repositories[repoIndex] = updatedRepo;
+
+  return response.json(updatedRepo);
 });
 
-app.delete("/repositories/:id", (req, res) => {
-  // TODO
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+
+  repoIndex = repositories.findIndex(repo => repo.id === id);
+  if (repoIndex < 0) {
+    return response.status(400).send();
+  }
+  repositories.splice(repoIndex, 1);
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  repoIndex = repositories.findIndex(repo => repo.id === id);
+  if (repoIndex < 0) {
+    return response.status(400).send();
+  }
+  const repo = repositories[repoIndex];
+  repo.likes += 1;
+  repositories[repoIndex] = repo;
+
+  return response.json({likes: repo.likes});
 });
 
 module.exports = app;
